b10d6289f53f7d418829a207d1ba59cae09f49d3
GustavoRizerioDev <gustavo.rsouza@sptech.school>
2025-01-21 10:30:28 -0300

    :sparkles: feat: Exercicios resolvidos

--- exercicios/src/main/java/com/estudos/searchInsertPosition/SearchInsertPosition.java
+++ exercicios/src/main/java/com/estudos/searchInsertPosition/SearchInsertPosition.java
@@ -1,4 +1,22 @@
 package com.estudos.searchInsertPosition;
 
 public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int middle = 0;
+        while (left <= right) {
+            middle = left + (right - left) / 2;
+            if (nums[middle] == target) {
+                return middle;
+            }
+            if (nums[middle] < target) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+        return left;
+    }
+
 }
